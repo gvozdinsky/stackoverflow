d6e6fe006f2382d9d61f94082729de664fa77fc9
Dmitrii GVOZDINSCHI <dgvozdinschi@pentalog.com>
2019-06-03 10:46:11 +0300

    create AnswerList component

---
+++ src/components/answer/AnswerList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div v-for="answer in answers"
+         :key="answer.id"
+         v-html="answer.body"
+         :class="{ accepted: answer.is_accepted }"
+         class="answer"
+    >
+    </div>>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    answers: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .answer {
+    padding: 10px 15px;
+    background-color: #f4f4f4;
+    margin-bottom: 30px;
+
+    &.accepted {
+      background-color: #ecfbec;
+    }
+  }
+</style>
