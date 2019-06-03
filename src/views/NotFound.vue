2e9e57b0b3fbd707627a505983a0fd2b69979abb
Dmitrii GVOZDINSCHI <dgvozdinschi@pentalog.com>
2019-06-03 14:38:07 +0300

    create NotFound page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center">
+    <h1>404</h1>
+    <h2>The {{ resource }} you're looking for is not here</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      default: 'page'
+    }
+  }
+}
+</script>
+
+<style>
+  .center {
+    text-align: center;
+  }
+</style>
