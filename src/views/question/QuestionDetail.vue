3a5c6b5b26f944dff1308980f57eccf5168792cd
Dmitrii GVOZDINSCHI <dgvozdinschi@pentalog.com>
2019-06-03 10:47:32 +0300

    add AnswerList on QuestionDetail page

--- src/views/question/QuestionDetail.vue
+++ src/views/question/QuestionDetail.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <h1> {{ question. title }}</h1>
     <pre>{{ question }}</pre>
+    <div>
+      <h2>Answers:</h2>
+      <AnswerList :answers="answers" />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import AnswerList from '@/components/answer/AnswerList'
 export default {
   name: 'question-detail',
   props: {
@@ -13,14 +19,20 @@ export default {
       required: true
     }
   },
+  components: {
+    AnswerList
+  },
   methods: {
-    ...mapActions('questions', ['getQuestion'])
+    ...mapActions('questions', ['getQuestion']),
+    ...mapActions('answers', ['getAnswersForQuestion'])
   },
   computed: {
-    ...mapState('questions', ['question'])
+    ...mapState('questions', ['question']),
+    ...mapState('answers', ['answers'])
   },
   mounted () {
     this.getQuestion(this.id)
+    this.getAnswersForQuestion(this.id)
   }
 }
 </script>
