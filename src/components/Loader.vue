812bc0f7562626fedbf4b5402967d94a7f09dab2
Dmitrii GVOZDINSCHI <dgvozdinschi@pentalog.com>
2019-06-06 09:40:02 +0300

    create Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,20 @@
+<template>
+  <fragment>
+    <v-layout align-center justify-center v-if="loading">
+      <v-progress-circular size="80" color="primary" indeterminate />
+    </v-layout>
+    <slot v-if="!loading"></slot>
+  </fragment>
+</template>
+
+<script>
+export default {
+  name: 'loader',
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
